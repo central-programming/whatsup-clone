5792f205aff9eccb8f51ea7347dc8253b8e38fc0
EJ <38365682+e-rojas@users.noreply.github.com>
2023-06-12 09:44:33 -0300

    base fonts
    base files and fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const counter = new Counter(count, setCount)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+      <Text>The score is: {counter.getCount()}</Text>
+      <Button title="Increase" onPress={counter.increment} />
+      <Button title="Decrease" onPress={counter.decrement} />
       <StatusBar style="auto" />
-    </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +22,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
+
+class Counter {
+  count: number
+  setCount: React.Dispatch<React.SetStateAction<number>>
+  constructor(count: number, setCount: React.Dispatch<React.SetStateAction<number>>) {
+    this.count = count
+    this.setCount = setCount
+  }
+  increment = () => {
+    // this.setCount(this.count + 1)
+    this.setCount((prevCount) => prevCount + 1)
+  }
+  decrement = () => {
+    // this.setCount(this.count - 1)
+    this.setCount((prevCount) => prevCount - 1)
+  }
+  getCount() {
+    return this.count
+  }
+
+}
