b9933c069edc78db7764441c324eead60be82c69
EJ <136189952+central-programming@users.noreply.github.com>
2023-06-12 19:13:26 -0300

    basic chat components created

--- screens/chat.tsx
+++ screens/chat.tsx
@@ -1,11 +1,29 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ImageBackground, TouchableOpacity } from "react-native";
 import { ChatScreenProps } from "../types/navigator";
+import { Asset } from 'expo-asset';
+import { styles } from "../styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-gesture-handler";
+import { Feather } from '@expo/vector-icons';
+const bg = Asset.fromModule(require('../assets/images/chat-bg.png')).uri;
 
-export default function ChatListScreen({navigation}:ChatScreenProps) {
+export default function ChatScreen({ navigation }: ChatScreenProps) {
     return (
-        <View>
-            <Text>Chat Screen</Text>
-            <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-        </View>
+        <SafeAreaView
+            edges={['right', 'bottom', 'left']}
+            style={[styles.flex1, styles.column]}
+        >
+            <ImageBackground source={{ uri: bg }} style={[styles.flex1]}>
+            </ImageBackground>
+            <View style={[styles.row, styles.chatFooterBar, styles.spread]}>
+                <TouchableOpacity onPress={() => { console.log('pressed') }} style={[styles.xCenter,styles.yCenter]}>
+                    <Feather name="plus" size={24} style={styles.colorAlert} />
+                </TouchableOpacity>
+                <TextInput placeholder="Type a message"  style={[styles.flex1, styles.chatFooterTextBox]}/>
+                <TouchableOpacity onPress={() => { console.log('pressed') }} style={[styles.xCenter,styles.yCenter]}>
+                    <Feather name="camera" size={24} style={styles.colorAlert} />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 }
